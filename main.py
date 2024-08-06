0d908c5a6b334be577b8a62ae1b68bd152a8996c
unknown <gabrielswagner7@gmail.com>
2024-08-06 17:06:52 -0500

    meteor class/rect

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame, sys 
+from random import randint, uniform
 
 class Ship(pygame.sprite.Sprite):
 
@@ -43,6 +44,21 @@ class Laser(pygame.sprite.Sprite):
     def update(self):
         self.pos += self.direction * self.speed * dt
         self.rect.topleft = (round(self.pos.x),round(self.pos.y))
+
+class Meteor(pygame.sprite.Sprite):
+    def __init__(self, pos, groups):
+        super().__init__(groups)
+        self.image = pygame.image.load('./graphics/meteor.png').convert_alpha()
+        self.rect = self.image.get_rect(center = pos)
+
+        self.pos = pygame.math.Vector2(self.rect.topleft)
+        self.direction = pygame.math.Vector2(uniform(-0.5, 0.5), 1)
+        self.speed = randint(400, 600)
+    
+    def update(self):
+        self.pos += self.direction * self.speed * dt
+        self.rect.topleft = (round(self.pos.x),round(self.pos.y))
+
     
 
 # game setup
@@ -59,9 +75,14 @@ background_surf = pygame.image.load('./graphics/background.png').convert()
 #sprite groups
 spaceship_group = pygame.sprite.GroupSingle()
 laser_group = pygame.sprite.Group()
+meteor_group = pygame.sprite.Group()
 
 # sprite creation
 ship = Ship(spaceship_group)
+
+# timer
+meteor_timer = pygame.event.custom_type()
+pygame.time.set_timer(meteor_timer, 400)
 
 
 # game loop
@@ -70,6 +91,10 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        if event.type == meteor_timer:
+            meteor_y_pos = randint(-150, -50)
+            meteor_x_pos = randint(-100, WINDOW_WIDTH + 100)
+            Meteor((meteor_x_pos, meteor_y_pos), groups=meteor_group)
 
     #delta time
     dt = clock.tick() / 1000
@@ -80,10 +105,12 @@ while True:
     # update
     spaceship_group.update()
     laser_group.update()
+    meteor_group.update()
 
     #graphics
     spaceship_group.draw(display_surface)
     laser_group.draw(display_surface)
+    meteor_group.draw(display_surface)
     
 
     pygame.display.update()
