1145a2316ce0b0ba90c5f2556a377a3c81bc9ef7
unknown <gabrielswagner7@gmail.com>
2024-08-07 15:06:36 -0500

    bug fix/pygame masks

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ class Ship(pygame.sprite.Sprite):
         super().__init__(groups)
         self.image = pygame.image.load('./graphics/cat_ship.png').convert_alpha()
         self.rect = self.image.get_rect(center = (WINDOW_WIDTH /2, WINDOW_HEIGHT / 2))
+        self.mask = pygame.mask.from_surface(self.image)
         self.can_shoot = True
         self.shoot_time = None
 
@@ -21,29 +22,44 @@ class Ship(pygame.sprite.Sprite):
                 self.can_shoot = True
 
     def laser_shoot(self):
-        if not pygame.mouse.get_pressed()[0] and self.can_shoot:
+        if pygame.mouse.get_pressed()[0] and self.can_shoot:
             self.can_shoot = False
             self.shoot_time = pygame.time.get_ticks()
             Laser(self.rect.midtop, laser_group)
-          
+    
+    def meteor_collisions(self):
+       if pygame.sprite.spritecollide(self, meteor_group, False, pygame.sprite.collide_mask):
+           pygame.quit()
+           sys.exit
+
     def update(self):
         self.laser_timer()
         self.laser_shoot()
         self.input_position()
+        self.meteor_collisions()
 
 class Laser(pygame.sprite.Sprite):
     def __init__(self, pos, groups):
         super().__init__(groups)
         self.image = pygame.image.load('./graphics/laser.png').convert_alpha()
         self.rect = self.image.get_rect(midbottom = pos)
+        self.mask = pygame.mask.from_surface(self.image)
 
         self.pos = pygame.math.Vector2(self.rect.topleft)
         self.direction = pygame.math.Vector2(0, -1)
         self.speed = 600
+
+    def meteor_collision(self):
+        if pygame.sprite.spritecollide(self, meteor_group, True, pygame.sprite.collide_mask):
+            self.kill()
+
     
     def update(self):
         self.pos += self.direction * self.speed * dt
         self.rect.topleft = (round(self.pos.x),round(self.pos.y))
+        if self.rect.bottom < 0:
+            self.kill()
+        self.meteor_collision()
 
 class Meteor(pygame.sprite.Sprite):
     def __init__(self, pos, groups):
@@ -53,6 +69,7 @@ class Meteor(pygame.sprite.Sprite):
         self.scaled_surf = pygame.transform.scale(meteor_surf, meteor_size)
         self.image = self.scaled_surf 
         self.rect = self.image.get_rect(center = pos)
+        self.mask = pygame.mask.from_surface(self.image)
 
         self.pos = pygame.math.Vector2(self.rect.topleft)
         self.direction = pygame.math.Vector2(uniform(-0.5, 0.5), 1)
@@ -67,11 +84,15 @@ class Meteor(pygame.sprite.Sprite):
         rotated_surf = pygame.transform.rotozoom(self.scaled_surf, self.rotation, 1)
         self.image = rotated_surf
         self.rect = self.image.get_rect(center = self.rect.center)
+        self.mask = pygame.mask.from_surface(self.image)
     
     def update(self):
         self.pos += self.direction * self.speed * dt
         self.rect.topleft = (round(self.pos.x),round(self.pos.y))
         self.rotate()
+
+        if self.rect.top > WINDOW_HEIGHT:
+            self.kill()
 
 class Score:
     def __init__(self):
